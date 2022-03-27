881976dac92729fa9e3707535053d931531f127a
John <nadenjohn@gmail.com>
2022-03-27 15:56:18 -0700

    added skeleton and rotate bikes

--- mototours/src/App.js
+++ mototours/src/App.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import './components/Skeleton-2.0.4 2/css/skeleton.css';
+import './components/Skeleton-2.0.4 2/css/normalize.css';
 import Header from './components/Header'
 import InfoContainer from './components/InfoContainer'
 import MotorcycleContainer from './components/MotorcycleContainer'
+
+const display = 5;
 
 
 function App() {
 const API = "http://localhost:3000/motor";
 
 const [ motor, setMotor ] = useState ([]);
+const [startBikes, setStartBikes] = useState([]);
 
 
 useEffect(() => {
@@ -19,13 +24,16 @@ useEffect(() => {
   });
 }, []);
 
+function nextBikes() {
+  setStartBikes(startBikes + display)
+}
 
 
   return (
-    <div className="App">
+    <div className="container">
     <Header />
     <InfoContainer />
-    <MotorcycleContainer motor={motor} />
+    <MotorcycleContainer motor={motor.slice(startBikes, startBikes + display)} nextBikes={nextBikes} />
     </div>
   );
 };
