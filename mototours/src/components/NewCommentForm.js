fad38ea17ac574fde988cb736879498d9e3079a7
Michelle Caraballo <caraballomp@yahoo.com>
2022-03-30 09:17:36 -0700

    adding patch ad info card

--- mototours/src/components/NewCommentForm.js
+++ mototours/src/components/NewCommentForm.js
@@ -1,41 +1,49 @@
 import React, {useState} from "react";
 
-function NewCommentForm() {
+function NewCommentForm({moto}) {
 
-const [comment, setComment] = useState("");
 
+const [comment, setComment] = useState();
 
 
 function handleComment(e) {
-      setComment(e.target.value);
+  setComment(e.target.value);
+  // console.log("ID", moto.id)
+  // console.log("COMMENT: ", moto.Comment)
     }
 
     const handleSubmit = (e) => {
-      e.preventDefault()
+      e.preventDefault();
 
-      const formData ={
-      comment,
+      const formData = {
+      id: `${moto}`,
+      Comment: `${comment}`
       }
-      fetch("http://localhost:3001/motor", {
-        method: 'POST',
-        headers:{
-          'Content-Type':'application/json'
+      fetch(`http://localhost:3001/motor/${moto.id}`, {
+        method: 'PATCH',
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData)  
       })
       .then((r) => r.json())
-      .then(setComment())
-      };
+        .then(data => setComment(comment));
+      }
     
+  
 
     return (
       <form className="" onSubmit={handleSubmit} >
+      <label>Review</label>
   <textarea
           placeholder="Write experience here"
           rows={10}
           onChange={handleComment}
         />
         <input type="submit" value="Share your experience" />
+        <p>{ comment }</p>  
+        
       </form>
     );
     }
